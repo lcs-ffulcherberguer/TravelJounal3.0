48e3e701adf46f5a7762739ae5c50724c1514594
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2021-03-03 11:48:52 -0500

    Added Assets for tab. Started working on tabView

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        Home()
     }
 }
 
@@ -19,3 +19,121 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Home : View {
+    var body: some View {
+        
+        VStack{
+            
+            
+            Spacer()
+            
+            Text("Home")
+                
+            Spacer()
+            
+        }
+        
+    }
+}
+
+struct Map : View {
+    var body: some View {
+        
+        VStack{
+            
+            
+            Spacer()
+            
+            Text("Map")
+                
+            Spacer()
+            
+        }
+        
+    }
+}
+
+struct Journal : View {
+    var body: some View {
+        
+        VStack{
+            
+            
+            
+            Spacer()
+            
+            Text("Journal")
+                
+            Spacer()
+            
+        }
+        
+    }
+}
+
+//Creating tab view
+
+struct MainView : View {
+    
+    
+    @State var tab = "Explore"
+    var body: some View {
+        
+        VStack(spacing: 0) {
+            
+            Spacer()
+            
+            HStack(spacing:0) {
+                
+                tabButton(title: "Home", tab: $tab)
+                
+                Spacer(minLength: 0)
+                
+                tabButton(title: "Map", tab: $tab)
+                
+                Spacer(minLength: 0)
+                
+                tabButton(title: "Journal", tab: $tab)
+                
+                
+                
+            }
+            .padding(.top)
+            //for smaller size Iphones
+            .padding(.bottom, UIApplication.shared.windows.first!.safeAreaInsets.bottom == 0 ? 15 : UIApplication.shared.windows.first!.safeAreaInsets.bottom)
+            
+        }
+       
+        .edgesIgnoringSafeArea(.all)
+        .background(Color("bg").edgesIgnoringSafeArea(.all))
+    }
+    
+}
+
+
+//Tab Buttons
+
+struct tabButton : View {
+    
+    var title : String
+    @Binding var tab : String
+    
+    var body: some View {
+    
+    Button(action: {tab = title}){
+        
+        Image(title)
+            .renderingMode(.original)
+            .foregroundColor(tab == title ? .white : .gray)
+            .padding(.vertical,10)
+            .padding(.horizontal)
+            .background(Color("Color").opacity(tab == title ? 1 : 0))
+            .clipShape(Capsule())
+        
+       }
+    
+    }
+   
+}
+
